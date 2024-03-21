324130d4d3b5c7a07cf1a2d8bf5fee2d8348e7ef
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-03-21 17:35:05 -0300

    feat: creation molde PlanetManagement

--- src/screens/PlanetManagement/index.jsx
+++ src/screens/PlanetManagement/index.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function Management() {
-  return (
-    <View>
-      <Text>Management</Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={styles.pageDetails}>
+                <Text>Gerenciamento de Mundos</Text>
+                <Text>Bem-vindo ao centro de controle intergaláctico! Aqui, você tem o poder de moldar o universo ao seu gosto.
+                    Adicione novos mundos exóticos com um toque, ou edite os existentes para refletir as mudanças cósmicas.</Text>
+                <TouchableOpacity>
+                    <Text>Adicionar Mundo</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.containerInserts}>
+
+            </View>
+        </View>
+    )
 }
