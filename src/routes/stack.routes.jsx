34217146e822e18ecbb21e5b12c74bd69218d741
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-03-31 11:39:22 -0300

    fix: drawer

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,7 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
-import Profile from "../screens/Profile";
 import Planet from "../screens/Planet";
 
 const Stack = createNativeStackNavigator();
@@ -10,7 +9,6 @@ const StackRoutes = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Planet" component={Planet} />
     </Stack.Navigator>
   );
